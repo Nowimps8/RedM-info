e7ec99b0555511062bc9e409e8e56315c66a8d93
Nowimps8 <31818901+Nowimps8@users.noreply.github.com>
2020-05-23 14:41:33 -0600

    Create emotes.lua

---
+++ emotes.lua
@@ -0,0 +1,156 @@
+					case 15563:
+						return 1927505461 /* GXTEntry: "Blow Kiss" */;
+					case 15564:
+						return -1252070669 /* GXTEntry: "Boast" */;
+					case 15565:
+						return -667707842;
+					case 15566:
+						return -402959 /* GXTEntry: "Hell Yeah" */;
+					case 15567:
+						return -773960361 /* GXTEntry: "Flex" */;
+					case 15568:
+						return 1115379199 /* GXTEntry: "Follow Me" */;
+					case 15569:
+						return 796723886 /* GXTEntry: "Hissy Fit" */;
+					case 15570:
+						return 877603501;
+					case 15571:
+						return -415456998 /* GXTEntry: "Let\'s Craft" */;
+					case 15572:
+						return 1159716480 /* GXTEntry: "Wanna Fish?" */;
+					case 15573:
+						return 1593752891 /* GXTEntry: "Let\'s Go" */;
+					case 15574:
+						return -843470756 /* GXTEntry: "Play Some Cards" */;
+					case 15575:
+						return 935157006 /* GXTEntry: "Look to Distance" */;
+					case 15576:
+						return 7918540 /* GXTEntry: "Look Yonder" */;
+					case 15577:
+						return -919436740;
+					case 15578:
+						return 486225122 /* GXTEntry: "Point" */;
+					case 15579:
+						return 474409519 /* GXTEntry: "Posse Up" */;
+					case 15580:
+						return 1307697675 /* GXTEntry: "Prospector Jig" */;
+					case 15581:
+						return 589481092 /* GXTEntry: "Scheme" */;
+					case 15582:
+						return -1639456476 /* GXTEntry: "Shoot \'Em Up" */;
+					case 15583:
+						return 1939251934 /* GXTEntry: "Shoot the Sky" */;
+					case 15584:
+						return -2124325981;
+					case 15585:
+						return -1954574613;
+					case 15586:
+						return -2106738342 /* GXTEntry: "Spit" */;
+					case 15587:
+						return -1691237868 /* GXTEntry: "Let\'s Stop Here" */;
+					case 15588:
+						return -293533575;
+					case 15589:
+						return -256960614;
+					case 15590:
+						return -632925771;
+					case 15591:
+						return -19706765;
+					case 15592:
+						return -1790393337;
+					case 15593:
+						return 1043523184;
+					case 15594:
+						return -2072898350;
+					case 15595:
+						return -808997190;
+					case 15596:
+						return 217596073;
+					case 15597:
+						return 1140268200;
+					case 15598:
+						return -2121881035 /* GXTEntry: "Fancy Bow" */;
+					case 15599:
+						return 901097731 /* GXTEntry: "Gentle Wave" */;
+					case 15600:
+						return -1666834415;
+					case 15601:
+						return 523585988 /* GXTEntry: "Good to See Ya" */;
+					case 15602:
+						return -511010399;
+					case 15603:
+						return -1457020913 /* GXTEntry: "Hat Tipping" */;
+					case 15604:
+						return 831975651 /* GXTEntry: "Hey You!" */;
+					case 15605:
+						return -1801715172 /* GXTEntry: "Respectful Bow" */;
+					case 15606:
+						return 1018554126;
+					case 15607:
+						return -1551032732;
+					case 15608:
+						return -462132925 /* GXTEntry: "Ta-Da!" */;
+					case 15609:
+						return 425751659 /* GXTEntry: "Thumbs Up" */;
+					case 15610:
+						return 1879954891 /* GXTEntry: "Tough Guy Nod" */;
+					case 15611:
+						return -339257980 /* GXTEntry: "Wave" */;
+					case 15612:
+						return -221241824 /* GXTEntry: "Applause" */;
+					case 15613:
+						return 164860528 /* GXTEntry: "Beg for Mercy" */;
+					case 15614:
+						return -934299978;
+					case 15615:
+						return -78874669;
+					case 15616:
+						return 1802342943 /* GXTEntry: "How Dare You" */;
+					case 15617:
+						return -1118911493 /* GXTEntry: "Gag" */;
+					case 15618:
+						return -1644757697 /* GXTEntry: "Hush Your Mouth" */;
+					case 15619:
+						return 296809845;
+					case 15620:
+						return -822629770 /* GXTEntry: "Nod Head" */;
+					case 15621:
+						return 803206066 /* GXTEntry: "Point and Laugh" */;
+					case 15622:
+						return -653113914 /* GXTEntry: "Shake Head" */;
+					case 15623:
+						return -110352861 /* GXTEntry: "I\'m Hit" */;
+					case 15624:
+						return -1000272583;
+					case 15625:
+						return 1023735814 /* GXTEntry: "Slow Clap" */;
+					case 15626:
+						return 81347960 /* GXTEntry: "Weep" */;
+					case 15627:
+						return -1023149885;
+					case 15628:
+						return 1509171361 /* GXTEntry: "Thumbs Down" */;
+					case 15629:
+						return 329631369 /* GXTEntry: "Who, Me?" */;
+					case 15630:
+						return 445839715 /* GXTEntry: "Yeehaw" */;
+					case 15631:
+						return 246916594 /* GXTEntry: "You Gonna Cry?" */;
+					case 15632:
+						return 1825177171 /* GXTEntry: "Chicken" */;
+					case 15633:
+						return 969312568 /* GXTEntry: "Flip Off" */;
+					case 15634:
+						return -553424129 /* GXTEntry: "I\'m Watching You" */;
+					case 15635:
+						return 1533402397 /* GXTEntry: "Bring It On" */;
+					case 15636:
+						return 1256841324 /* GXTEntry: "Throat Slit" */;
+					case 15637:
+						return 354512356 /* GXTEntry: "Damn You" */;
+					case 15638:
+						return -2104721573;
+					case 15639:
+						return 987239450 /* GXTEntry: "War Cry" */;
+					case 15640:
+						return -166523388 /* GXTEntry: "What\'s The Smell?" */;
